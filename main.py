3818b6d399478bfbf21f34c2088a14bef6242ebd
DIGANTA CHAUDHURI <73929709+DigantaChauduri06@users.noreply.github.com>
2021-12-01 10:26:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import os
 import long_responses as long
 from threading import Timer
+import pymongo
+from datetime import datetime
 
 
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
@@ -29,7 +31,15 @@ def message_probability(user_message, recognised_words, single_response=False, r
     else:
         return 0
 
+
 # ------------------------------------------------------------------------------------------------
+# Profiles
+R_LEETCODE = "https://leetcode.com/DigantaC/"
+R_GFG = "https://auth.geeksforgeeks.org/user/digantachaudhuri03/profile"
+R_LINKEDIN = "https://www.linkedin.com/in/digantachaudhuri06"
+R_FACEBOOK = "https://www.facebook.com/diganta.chaudhuri.395"
+R_JOKES= long.a
+
 
 def check_all_messages(message):
     highest_prob_list = {}
@@ -40,11 +50,21 @@ def check_all_messages(message):
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
     # Responses -------------------------------------------------------------------------------------------------------
+
     response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
     response('See you!', ['bye', 'goodbye'], single_response=True)
     response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
     response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    response('HERE YOUR LEETCODE PROFILE \n' + R_LEETCODE + '\n', ['show', 'your', 'leetcode', 'profile'],
+             required_words=['leetcode'])
+    response('HERE YOUR LINKEDIN PROFILE \n' + R_LINKEDIN + '\n', ['show', 'your', 'linkedin', 'profile'],
+             required_words=['linkedin'])
+    response('HERE YOUR FACEBOOK PROFILE \n' + R_FACEBOOK + '\n', ['show', 'your', 'facebook', 'profile'],
+             required_words=['facebook'])
+    response('HERE YOUR GFG PROFILE \n' + R_GFG + '\n', ['show', 'your', 'gfg', 'profile'], required_words=['gfg'])
+    response('HERE YOUR JOKE ENJOY 😁 \n' + R_JOKES, ['tell', 'me', 'a', 'joke'], required_words=['joke'])
 
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
@@ -62,11 +82,45 @@ def get_response(user_input):
     return response
 
 
+List = []
+details = []
+
+
 # Testing the response system
 def func():
-    while True:
-        print('Bot: ' + get_response(input('You: ')))
+    name = input('Enter Your Name: ')
+    email = input('Enter Your Email: ')
+    age = input('Enter Your Age: ')
+    details.append(name)
+    details.append(email)
+    details.append(int(age))
 
+    while True:
+        user = input('You: ')
+        str = get_response(user)
+        List.append(user)
+        if user == 'exit' or user == 'quit':
+            dbWork(details, List)
+            break
+        print('Bot: ' + str)
+
+#DB work
+def dbWork(details, conv):
+    # try:
+    client = pymongo.MongoClient('mongodb+srv://Diganta:Diganta123@cluster0.wcrjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    mydb = client["mydb"]
+    mycol=mydb['python project']
+    now = datetime.now()
+    data = {
+        'name': details[0],
+        'email': details[1],
+        'age': details[2],
+        'conversions': conv,
+        'Time': now.strftime("%H:%M:%S")
+    }
+    mycol.insert_one(data)
+    # except:
+    #     print("An exception occurred")
 
 # func()
 print('##################################################')
