8c549ee4a1929434f6fb2934dfee055a1dae618c
DIGANTA CHAUDHURI <73929709+DigantaChauduri06@users.noreply.github.com>
2021-12-01 10:25:15 +0530

    Update long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -1,13 +1,30 @@
 import random
+import requests
+import json
 
 R_EATING = "I don't like eating anything because I'm a bot obviously!"
 R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
 
 
+
 def unknown():
-    response = ["Could you please re-phrase that? ",
+    response = ["Its better if you can search this thing on google ",
                 "...",
                 "Sounds about right.",
                 "What does that mean?"][
         random.randrange(4)]
     return response
+
+
+def jokes(f):
+    try:
+        data = requests.get(f)
+        tt = json.loads(data.text)
+        if 'joke' in tt.keys():
+            return tt['joke']
+        return tt['setup'] + ' ' + tt['delivery']
+    except:
+        return 'Error Happen while retreving joke'
+
+f = r"https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas"
+a = jokes(f)
